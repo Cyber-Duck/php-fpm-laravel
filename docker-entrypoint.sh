f695c5e2add92ad600ad2fb54e23dd777f18387f
Simone Todaro <simone@cyber-duck.co.uk>
2018-06-20 19:40:12 +0100

    Disable XDebug if container option is set to false

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,10 @@
 
 service cron start
 
+# Toggle xdebug
+if [ "false" == "$XDEBUG" ]; then
+    sed -i "s/^/;/" /usr/local/etc/php/conf.d/xdebug.ini
+    sed -i "s/^/;/" /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+fi
+
 exec "$@"
